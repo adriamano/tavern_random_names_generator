0212cb0fc89455b3abe8c5290e108b2d57d599b2
adrimano <adri.duarte1995@gmail.com>
2023-04-03 21:46:22 -0300

    um tendel de coisa

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 const firstNames = ["Beholder", "Anão", "Elfo", "Orc", "Kua-Toa", "Bárbaro", "Lich", "Ninfa", "Unicórnio", "Pergaminho", "Javalí", "Mendigo", "Dragonete", "Bardo", "Bruxa", "Dragão", "Urso", "Elemental", "Peixe", "Tolo"];
 
-const secondNames = ["O bom", "Cabeça de","O descanso do", "Os irmãos", "Filha de", "Olho de", "Tesouro do", "Banquete dos", "A Fera e o", "A lenda dos", "Covil dos", "A espada do", "O devorador de", "Caldeirão do", "Barriga de", "A princesa e o", "Os treze", "Taverna do", "Punho de", "Amigo dos"];
+const secondNames = ["O bom", "Cabeça de", "O descanso do", "Os irmãos", "Filha de", "Olho de", "Tesouro do", "Banquete dos", "A Fera e o", "A lenda dos", "Covil dos", "A espada do", "O devorador de", "Caldeirão do", "Barriga de", "A princesa e o", "Os treze", "Taverna do", "Punho de", "Amigo dos"];
 
 const thirdNames = ["Caolho", "Bêbado", "Sorridente", "Fiel", "Flatulento", "Ancião", "Perigoso", "Morto-vivo", "Prometido", "Perfumado", "Encantado", "Gordo", "Tarado", "Furioso", "Brigão", "Cantante", "Ferido", "Exausto", "Eterno", "Cotoco"];
 
@@ -9,33 +10,32 @@ const getRandomNumber = (max) => Math.floor(Math.random() * max);
 const getRandomName1 = () =>
     `${secondNames[getRandomNumber(secondNames.length)]} ${firstNames[getRandomNumber(firstNames.length)]} `;
 
-    const getRandomName2 = () =>
+const getRandomName2 = () =>
     `${firstNames[getRandomNumber(firstNames.length)]} ${thirdNames[getRandomNumber(thirdNames.length)]}`;
 
-    const getRandomName3 = () =>
+const getRandomName3 = () =>
     `${secondNames[getRandomNumber(secondNames.length)]} ${firstNames[getRandomNumber(firstNames.length)]} ${thirdNames[getRandomNumber(thirdNames.length)]}`;
 
 
 const setRandomName1 = () => {
-    document.getElementById('random-name').innerText = getRandomName1();
+    document.getElementById('random_name').innerText = getRandomName1();
 }
 
 const setRandomName2 = () => {
-    document.getElementById('random-name').innerText = getRandomName2();
+    document.getElementById('random_name').innerText = getRandomName2();
 }
 
 const setRandomName3 = () => {
-    document.getElementById('random-name').innerText = getRandomName3();
+    document.getElementById('random_name').innerText = getRandomName3();
 }
 
 
 document.getElementById('generate1')
     .addEventListener('click', setRandomName1);
 
-    document.getElementById('generate2')
+document.getElementById('generate2')
     .addEventListener('click', setRandomName2);
 
-    document.getElementById('generate3')
+document.getElementById('generate3')
     .addEventListener('click', setRandomName3);
 
-setRandomName();
